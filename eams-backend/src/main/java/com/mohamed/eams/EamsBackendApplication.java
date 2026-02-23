ea22571c0ce2c81ad7df420fad371a1150cb4c15
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-02-23 22:04:39 +0100

    first commit

---
+++ eams-backend/src/main/java/com/mohamed/eams/EamsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.eams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamsBackendApplication.class, args);
+	}
+
+}
---
+++ eams-backend/src/test/java/com/mohamed/eams/EamsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.eams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
