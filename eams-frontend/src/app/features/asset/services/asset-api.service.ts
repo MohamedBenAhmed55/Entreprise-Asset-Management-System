b3b6267df6a80b87dbe3076a10836592990c2cdf
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-03-01 21:42:36 +0100

    fix backend port

--- eams-frontend/src/app/features/asset/services/asset-api.service.ts
+++ eams-frontend/src/app/features/asset/services/asset-api.service.ts
@@ -11,7 +11,7 @@ export class AssetApiService {
   private readonly http = inject(HttpClient);
 
   // TODO move to env file in the future
-  private readonly apiUrl = 'http://localhost:3000/api/v1/assets';
+  private readonly apiUrl = 'http://localhost:8080/api/v1/assets';
 
   /**
    * GET /api/v1/assets
